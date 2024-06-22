7b4105ed8f94554f3eb6ea2bfa3c48b493d580e6
Manikanth <65079433+alpha-neutr0n@users.noreply.github.com>
2024-06-22 18:43:31 +0530

    Update variables.tf

--- modules/local/variables.tf
+++ modules/local/variables.tf
@@ -6,16 +6,16 @@ variable "content" {
   default = "FO"
 }
 
-variable "test4" {
-  default = test4
+variable "test10"{
+  default = test1
 }
 
 variable "test3"{
   default = test3
 }
 
-variable "test10"{
-  default = test1
+variable "test4" {
+  default = test4
 }
 
 variable "length" {
